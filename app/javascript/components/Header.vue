ef6944d4afc914bd6f6d5b1836188ee48f2f75f4
将之大西 <omccello@gmail.com>
2019-08-17 16:50:27 +0900

    Headerの設定

---
+++ app/javascript/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+   <div>
+     <nav>
+       <div class="nav-wrapper">
+         <router-link to="/" class="brand-logo">Bookshelf</router-link>
+         <ul id="nav-mobile" class="right">
+           <li><router-link to="">本の登録</router-link></li>
+         </ul>
+       </div>
+     </nav>
+   </div>
+</template>
+
+<script></script>
+<style scoped></style>
