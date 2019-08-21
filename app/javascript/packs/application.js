f40209f270bdf298b44090fba73d2a883671d1f8
将之大西 <omccello@gmail.com>
2019-08-21 13:06:28 +0900

    Vuexの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,11 @@
 import Vue    from 'vue'
 import App    from './App.vue'
 import Router from '../router/router.js'
+import Store  from '../store/store.js'
 
 const app = new Vue({
    el: '#app',
    router: Router,
+   store: Store,
    render: h => h(App)
 })
