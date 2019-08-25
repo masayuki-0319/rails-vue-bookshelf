b49e53a3d9ae4806997e3e4607d7313f1d908b41
将之大西 <omccello@gmail.com>
2019-08-25 16:40:24 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :books, only: [:index, :show, :create, :update, :destroy]
+    resources :users,    only: [:create]
+    resources :sessions, only: [:create, :destroy]
   end
 end
