9c5f5403c0716d8538e2b14f59b49fec63c59a9b
将之大西 <omccello@gmail.com>
2019-08-25 20:41:44 +0900

    リフレッシュトークンの設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources :books, only: [:index, :show, :create, :update, :destroy]
     resources :users,    only: [:create]
+    resources :refresh,  only: [:create]
     resources :sessions, only: [:create, :destroy]
   end
 end
