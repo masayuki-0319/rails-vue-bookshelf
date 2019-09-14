64f3798dc9165a922f44a9ad403c722e1f755f62
将之大西 <omccello@gmail.com>
2019-09-14 16:21:10 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :books, only: [:index, :show, :create, :update, :destroy]
+    post   'signup',  controller: :users,    action: :create
+    post   'signin',  controller: :sessions, action: :create
+    delete 'signout', controller: :sessions, action: :destroy
+    post   'refresh', controller: :refresh,  action: :create
   end
 end
