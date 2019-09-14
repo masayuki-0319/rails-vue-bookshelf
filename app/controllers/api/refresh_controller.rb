700f9949641be203d2f1d5b02171940a31290de1
将之大西 <omccello@gmail.com>
2019-09-14 16:19:22 +0900

    リフレッシュトークンの設定

---
+++ app/controllers/api/refresh_controller.rb
@@ -0,0 +1,15 @@
+class Api::RefreshController < ApplicationController
+  before_action :authorize_refresh_by_access_request!
+
+  def create
+    session = JWTSessions::Session.new(payload: claimless_payload, refresh_by_access_allowed: true)
+    tokens = session.refresh_by_access_payload do
+      raise JWTSessions::Errors::Unauthorized, "Somethings not right here!"
+    end
+    response.set_cookie(JWTSessions.access_cookie,
+                        value: tokens[:access],
+                        httponly: true,
+                        secure: Rails.env.production?)
+    render json: { csrf: tokens[:csrf] }
+  end
+end
