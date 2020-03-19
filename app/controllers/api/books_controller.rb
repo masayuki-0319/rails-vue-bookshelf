8d5dc3d0510fea6d3bd3df980a560c0ea7331c1b
Masayuki Onishi <omccello@gmail.com>
2020-03-19 23:22:20 +0900

    rails6へのバージョンアップに係るエラー解消

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -3,12 +3,12 @@ class Api::BooksController < ApplicationController
 
   def index
     @books = Book.all
-    render 'index', formats: 'json', handlers: 'jbuilder'
+    render 'index', formats: :json, handlers: 'jbuilder'
   end
 
   def show
     @book = Book.find(params[:id])
-    render 'show', formats: 'json', handlers: 'jbuilder'
+    render 'show', formats: :json, handlers: 'jbuilder'
   end
 
   def create
